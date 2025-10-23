e0e90b130b18b3d8d95ab1a86d37b5807fe017f4
Javier <javiertorresmvm@gmail.com>
2025-10-23 12:54:35 -0500

    Presentacion WildHaven, formulario estatico

---
+++ PresentacionWildHaven/Pages/Models/ReporteSinRegistro.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Pages.Models
+{
+    public class ReporteSinRegistro
+    {
+        public string? name { get; set; }
+        public string? number { get; set; }
+        public string? typePet { get; set; }
+        public string? direcction { get; set; }
+        public string? addictionalInformation { get; set; } 
+
+    }
+}
